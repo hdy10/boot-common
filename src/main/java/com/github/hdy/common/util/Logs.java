b005074a2447db1c4f8f20f3a40030cdbcfd844d
hdy10 <498218711@qq.com>
2019-08-06 16:17:08 +0800

    集成spring-boot-starter-data-redis

--- src/main/java/com/github/hdy/common/util/Logs.java
+++ src/main/java/com/github/hdy/common/util/Logs.java
@@ -1,32 +1,33 @@
 package com.github.hdy.common.util;
 
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
+
+import lombok.extern.slf4j.Slf4j;
 
 /**
  * Created by hdy on 2017/10/17.
  */
+@Slf4j
 public class Logs {
-    private static final Logger logger = LogManager.getLogger(Logs.class.getName());
 
     public static void debug(Object msg) {
-        logger.debug("\u001b[33m" + msg + "\u001b[0m");
+        log.debug("\u001b[33m" + msg + "\u001b[0m");
     }
 
     public static void info(Object msg) {
-        logger.info("\u001b[35m" + msg + "\u001b[0m");
+        log.info("\u001b[35m" + msg + "\u001b[0m");
     }
 
 
     public static void warn(Object msg) {
-        logger.warn("\u001b[34m" + msg + "\u001b[0m");
+        log.warn("\u001b[34m" + msg + "\u001b[0m");
     }
 
     public static void error(Object msg) {
-        logger.error("\u001b[31m" + msg + "\u001b[0m");
+        log.error("\u001b[31m" + msg + "\u001b[0m");
     }
 
     public static void printHr(Object msg) {
         System.out.println("\u001b[33m" + msg + "\u001b[0m");
     }
+
 }
