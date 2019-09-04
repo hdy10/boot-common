0d128e947494971002f71f381573d6956f60f09a
hdy10 <498218711@qq.com>
2019-09-04 17:14:44 +0800

    Results 添加无参构造器

--- src/main/java/com/github/hdy/common/result/Results.java
+++ src/main/java/com/github/hdy/common/result/Results.java
@@ -14,6 +14,9 @@ public class Results {
     private String msg;
     private Object data;
 
+    public Results() {
+    }
+
     public Results(int code, String msg, Object data) {
         this.code = code;
         this.msg = msg;
